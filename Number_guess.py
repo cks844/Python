c78f2664e930750d009fe3dd7edfe43809cdc08d
CYRIL  K  SONY <84973835+cks844@users.noreply.github.com>
2022-10-18 23:26:29 +0530

    Update Number_guess.py

--- Number_guess.py
+++ Number_guess.py
@@ -1,15 +1,25 @@
 import random as rand
-num=rand.randint(1,10)
+num=rand.randint(0,10)
 guess = None
 Score = 10
-while guess!=num:
+while Score!=0:
+    if Score<=0:
+        print("Game over")
+        break
     guess=int(input("Enter a number in range 1-10:"))
     if guess == num:
         print("Congratulations,you have won the game")
         Score+=5
         print("Your score is:",Score)
-    else:
-        print("Sorry,please try again")
+    elif guess<num:
+        print("Your number is less than the random number")
+        Score+=-2
+        print("Your score is:",Score)
+        if Score<=0:
+            print("Game over")
+            break
+    elif guess>num:
+        print("Your number is greater than the random number")
         Score+=-2
         print("Your score is:",Score)
         if Score<=0:
