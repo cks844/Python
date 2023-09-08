b35f2d0c6f5e30b50cbcf65bad9f1b91aebf7a8d
CYRIL  K  SONY <84973835+cks844@users.noreply.github.com>
2023-09-08 13:27:55 +0530

    Update Add_sum.py

--- Add_sum.py
+++ Add_sum.py
@@ -5,11 +5,19 @@
 def addTwoNumbers(l1, l2):
     num1 = int(''.join(map(str, l1[::-1]))) #convert the list to integers
     num2 = int(''.join(map(str, l2[::-1])))
+    res=str(num1+num2)
+    print(res[::-1])
+l1 = [2,4,3]
+l2 = [5,6,4]
+add=addTwoNumbers(l1,l2)
+
+#Solution-2(for printing the sum as a list)Eg:[7,0,8]
+def addTwoNumbers(l1, l2):
+    num1 = int(''.join(map(str, l1[::-1]))) #convert the list to integers
+    num2 = int(''.join(map(str, l2[::-1])))
     s=str(num1+num2)
-    v=""
-    for i in s:
-        v=i+v
-    print(v)
+    res_list= [int(digit) for digit in res_list][::-1] 
+    print(res_list)
 l1 = [2,4,3]
 l2 = [5,6,4]
 add=addTwoNumbers(l1,l2)
