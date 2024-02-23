a453543f9937ac82ba535b30c2e54d52059ab0dd
CYRIL  K  SONY <84973835+cks844@users.noreply.github.com>
2024-02-23 13:41:55 +0530

    Update element_count.py

--- element_count.py
+++ element_count.py
@@ -1,10 +1,14 @@
 """Count the number of elements in a list
-Input: nums = [2,2,1,1,1,2,2,3]
-o/p:[1,3,4]
+Input: nums = [2,2,1,1,1,2,3,5,5]
+o/p:{2: 3, 1: 3, 3: 1, 5: 2}
 """
-nums = [2,2,1,1,1,2,2,3]
-new=[]
-for i in nums:
-    new.append(nums.count(i))
-new[:]=set(new)
-new
+def number_occurences(nums):
+    num_dict={}
+    for num in nums:
+        if num in num_dict:
+            num_dict[num]+=1
+        else:
+            num_dict[num]=1
+    return num_dict
+nums = [2,2,1,1,1,2,3,5,5]
+number_occurences(nums)    
